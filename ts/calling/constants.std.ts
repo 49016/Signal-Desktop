ccb103e09780418059df44dd3a12855fe0440618
49016 <sudo@linux.com>
2026-03-19 23:41:39 +0100

    Update video and screen share settings for higher quality

--- ts/calling/constants.std.ts
+++ ts/calling/constants.std.ts
@@ -4,17 +4,17 @@
 // See `TICK_INTERVAL` in group_call.rs in RingRTC
 export const AUDIO_LEVEL_INTERVAL_MS = 200;
 
-export const REQUESTED_VIDEO_WIDTH = 960;
-export const REQUESTED_VIDEO_HEIGHT = 540;
-export const REQUESTED_VIDEO_FRAMERATE = 30;
+export const REQUESTED_VIDEO_WIDTH = 1920;
+export const REQUESTED_VIDEO_HEIGHT = 1080;
+export const REQUESTED_VIDEO_FRAMERATE = 60;
 
-export const REQUESTED_GROUP_VIDEO_WIDTH = 640;
-export const REQUESTED_GROUP_VIDEO_HEIGHT = 360;
+export const REQUESTED_GROUP_VIDEO_WIDTH = 1280;
+export const REQUESTED_GROUP_VIDEO_HEIGHT = 720;
 
 export const REQUESTED_SCREEN_SHARE_WIDTH = 2880;
 export const REQUESTED_SCREEN_SHARE_HEIGHT = 1800;
 // 15fps is much nicer but takes up a lot more CPU.
-export const REQUESTED_SCREEN_SHARE_FRAMERATE = 5;
+export const REQUESTED_SCREEN_SHARE_FRAMERATE = 15;
 
 export const MAX_FRAME_WIDTH = 2880;
 export const MAX_FRAME_HEIGHT = 1800;
